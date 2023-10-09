c33f28f9cf606516898e9cfc90f3705602bfb2df
Jonathan <jonathanortega2023@gmail.com>
2023-10-09 05:33:51 +0000

    Refactored code for cleanliness, still errors

--- alderman_spending/lib/src/ui/menu_items_page/menu_items_screen.dart
+++ alderman_spending/lib/src/ui/menu_items_page/menu_items_screen.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
-import 'package:hovering/hovering.dart';
 import 'package:alderman_spending/src/data/models/menu_item_info.dart';
 import 'package:alderman_spending/src/data/loaders.dart';
 import 'package:intl/intl.dart';
 
-const baseImagePath = 'assets/images/menu_items/';
-final menuItems = loadMenuItems();
+const baseImagePath = 'assets/images/menu_items';
 
 class MenuItemsScreen extends StatelessWidget {
   const MenuItemsScreen({super.key});
@@ -14,167 +12,147 @@ class MenuItemsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Menu Items'),
-        ),
-        body: FutureBuilder(
-          future: menuItems,
-          builder: (context, AsyncSnapshot<List<MenuItemInfo>> snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  // MenuListItem that can be clicked to produce MenuItemDetailScreen
-                  // TODO, add routing or close button to MenuItemDetailScreen
-                  return InkWell(
-                    hoverColor: Colors.grey[300],
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => MenuItemDetailScreen(
-                            menuItemInfo: snapshot.data![index],
-                          ),
-                        ),
-                      );
-                    },
-                    child: MenuListItem(
-                      imagePath:
-                          baseImagePath + snapshot.data![index].imgFilename,
-                      title: snapshot.data![index].title,
-                      cost: snapshot.data![index].cost,
-                      unit: snapshot.data![index].measurement,
-                    ),
-                  );
-                },
-              );
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
-          },
-        ));
-  }
-}
+      appBar: AppBar(
+        title: const Text('Menu Items'),
+      ),
+      body: FutureBuilder<List<MenuItemInfo>>(
+        future: loadMenuItems(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return const Center(child: Text('Error loading data'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No data available'));
+          }
 
-class MenuItemDetailScreen extends StatelessWidget {
-  final MenuItemInfo menuItemInfo;
+          final menuItems = snapshot.data!;
 
-  const MenuItemDetailScreen({super.key, required this.menuItemInfo});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // TODO Make look better
-      body: Center(
-          child: GFCard(
-        elevation: 15,
-        padding: const EdgeInsets.all(20.0),
-        image: Image.asset(
-          baseImagePath + menuItemInfo.imgFilename,
-          fit: BoxFit.cover,
-        ),
-        showImage: true,
-        title: GFListTile(
-            title: Text(
-              menuItemInfo.title,
-              style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            subTitle: Text(
-              '${NumberFormat.simpleCurrency(decimalDigits: 0).format(menuItemInfo.cost)} per ${menuItemInfo.measurement}',
-              style: const TextStyle(fontSize: 16),
-            ),
-            shadow: BoxShadow(
-              color: Colors.grey.withOpacity(0.3),
-              spreadRadius: 5,
-              blurRadius: 7,
-              offset: const Offset(0, 3),
-            )),
-        // TODO Probably turn this into a scrollable text region
-        content: Column(
-          children: [
-            Text(
-              menuItemInfo.description,
-              style: const TextStyle(fontSize: 16),
-              overflow: TextOverflow.fade,
-            ),
-            if (menuItemInfo.notes != null)
-              ListView.builder(
-                shrinkWrap: true,
-                itemCount: menuItemInfo.notes!.length,
-                itemBuilder: (context, index) {
-                  return Text(
-                    'Note ${index + 1}: ${menuItemInfo.notes![index]}',
-                    style: const TextStyle(fontSize: 12),
-                  );
-                },
-              ),
-          ],
-        ),
-      )),
+          return ListView.builder(
+            itemCount: menuItems.length,
+            itemBuilder: (context, index) {
+              return MenuListItem(menuItemInfo: menuItems[index]);
+            },
+          );
+        },
+      ),
     );
   }
 }
 
 class MenuListItem extends StatelessWidget {
-  final String imagePath;
-  final String title;
-  final int cost;
-  final String unit;
+  final MenuItemInfo menuItemInfo;
 
-  const MenuListItem({
-    super.key,
-    required this.imagePath,
-    required this.title,
-    required this.cost,
-    required this.unit,
-  });
+  const MenuListItem({super.key, required this.menuItemInfo});
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      ItemCard(imagePath: imagePath, title: title, cost: cost, unit: unit),
-    ]);
-  }
-}
-
-class ItemCard extends StatelessWidget {
-  const ItemCard({
-    super.key,
-    required this.imagePath,
-    required this.title,
-    required this.cost,
-    required this.unit,
-  });
-
-  final String imagePath;
-  final String title;
-  final int cost;
-  final String unit;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.fromLTRB(5, 8, 5, 8),
-      elevation: 5,
-      child: ListTile(
-        dense: false,
-        leading: Image.asset(
-          imagePath,
-          fit: BoxFit.cover,
-          scale: 2,
-        ),
-        title: Text(
-          title,
-          style: const TextStyle(fontSize: 20),
-        ),
-        subtitle: Text(
-          "${NumberFormat.simpleCurrency(decimalDigits: 0).format(cost)} Per $unit",
-          style: const TextStyle(fontSize: 16),
+    return InkWell(
+      hoverColor: Colors.grey[300],
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MenuItemDetailScreen(
+              menuItemInfo: menuItemInfo,
+            ),
+          ),
+        );
+      },
+      child: Card(
+        margin: const EdgeInsets.fromLTRB(5, 8, 5, 8),
+        elevation: 5,
+        child: ListTile(
+          dense: false,
+          leading: Image.asset(
+            '$baseImagePath/${menuItemInfo.imgFilename}',
+            fit: BoxFit.cover,
+            scale: 2,
+          ),
+          title: Text(
+            menuItemInfo.title,
+            style: const TextStyle(fontSize: 20),
+          ),
+          subtitle: Text(
+            _parseCostMeasurement(menuItemInfo),
+            style: const TextStyle(fontSize: 16),
+          ),
         ),
       ),
     );
   }
+}
+
+class MenuItemDetailScreen extends StatelessWidget {
+  final MenuItemInfo menuItemInfo;
+  static final _boxShadowSettings = BoxShadow(
+    color: Colors.grey[500]!,
+    spreadRadius: 5,
+    blurRadius: 7,
+    offset: const Offset(0, 3),
+  );
+
+  const MenuItemDetailScreen({super.key, required this.menuItemInfo});
+  // TODO Add a back or close button
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GFCard(
+        height: MediaQuery.of(context).size.height,
+        elevation: 15,
+        // padding: const EdgeInsets.all(20.0),
+        image: Image.asset(
+          '$baseImagePath/${menuItemInfo.imgFilename}',
+          fit: BoxFit.cover,
+        ),
+        showImage: true,
+        title: GFListTile(
+          title: Text(
+            menuItemInfo.title,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          subTitle: Text(
+            _parseCostMeasurement(menuItemInfo),
+            style: const TextStyle(fontSize: 16),
+          ),
+          shadow: _boxShadowSettings,
+        ),
+        // margin: const EdgeInsets.all(20.0),
+        // TODO Make this scrollable, overflows for some reason
+        content: Column(
+          children: [
+            Text(
+              menuItemInfo.description,
+              style: const TextStyle(fontSize: 16),
+            ),
+            _parseNotes(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _parseNotes() {
+    if (menuItemInfo.notes != null) {
+      return Column(
+        children: menuItemInfo.notes!
+            .asMap()
+            .entries
+            .map((entry) => Text(
+                  'Note ${entry.key + 1}: ${entry.value}',
+                  style: const TextStyle(fontSize: 12),
+                ))
+            .toList(),
+      );
+    } else {
+      return const SizedBox.shrink();
+    }
+  }
+}
+
+_parseCostMeasurement(MenuItemInfo menuItemInfo) {
+  return '${NumberFormat.simpleCurrency(decimalDigits: 0).format(menuItemInfo.cost)} per ${menuItemInfo.measurement}';
 }
