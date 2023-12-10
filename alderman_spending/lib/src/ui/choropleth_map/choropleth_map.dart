07909f5bb9e6fe1131ffec5a6fc8ccab1ac4c30f
jonathan <jortega@pivotport.com>
2023-12-10 14:08:15 +0000

    Added asset path lookup for old boundaries

--- alderman_spending/lib/src/ui/choropleth_map/choropleth_map.dart
+++ alderman_spending/lib/src/ui/choropleth_map/choropleth_map.dart
@@ -11,16 +11,11 @@ import 'package:alderman_spending/src/data/providers/selected_data_choropleth.da
 import 'package:alderman_spending/src/data/models/map_ward_spending_data.dart';
 
 class ChoroplethMapPage extends StatelessWidget {
-  const ChoroplethMapPage ({super.key});
+  const ChoroplethMapPage({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // drawer: MyNavigationDrawer(),
-      // appBar: AppBar(
-      //   title: const Text('Map'),
-      // ),
       body: const _ChoroplethMap(),
     );
   }
@@ -33,7 +28,6 @@ class _ChoroplethMap extends StatefulWidget {
 }
 
 class __ChoroplethMapState extends State<_ChoroplethMap> {
-
   late List<MapWardSpendingData> _data;
   late MapShapeSource _dataSource;
   bool _isInitialized = false;
@@ -44,9 +38,13 @@ class __ChoroplethMapState extends State<_ChoroplethMap> {
   late List<MapWardSpendingData> _filteredMapData;
 
   Future<List<MapWardSpendingData>> _loadCSV() async {
-    final _rawData = await rootBundle.loadString("assets/spending_by_category.csv");
-    List<List<dynamic>> _listData = 
-    const CsvToListConverter().convert(_rawData, eol: '\n', shouldParseNumbers: false,);
+    final _rawData =
+        await rootBundle.loadString("assets/spending_by_category.csv");
+    List<List<dynamic>> _listData = const CsvToListConverter().convert(
+      _rawData,
+      eol: '\n',
+      shouldParseNumbers: false,
+    );
     List<MapWardSpendingData> spendingData = [];
     for (var i = 1; i < _listData.length; i++) {
       final item = _listData[i];
@@ -56,8 +54,12 @@ class __ChoroplethMapState extends State<_ChoroplethMap> {
         category: item[1],
         percent: double.parse(item[5].trim()),
       );
-      if (!_years.contains(newData.year)) {_years.add(newData.year);}
-      if (!_categories.contains(newData.category)) {_categories.add(newData.category);}
+      if (!_years.contains(newData.year)) {
+        _years.add(newData.year);
+      }
+      if (!_categories.contains(newData.category)) {
+        _categories.add(newData.category);
+      }
       spendingData.add(newData);
     }
     return spendingData;
@@ -65,7 +67,6 @@ class __ChoroplethMapState extends State<_ChoroplethMap> {
 
   @override
   void initState() {
-
     _loadCSV().then((data) {
       setState(() {
         _data = data;
@@ -94,53 +95,54 @@ class __ChoroplethMapState extends State<_ChoroplethMap> {
     if (_isInitialized) {
       return _data
           .where((element) =>
-              element.category == _selectedCategory && element.year == _selectedYear)
-          .toList();}
-    else {return [];}
+              element.category == _selectedCategory &&
+              element.year == _selectedYear)
+          .toList();
+    } else {
+      return [];
+    }
   }
 
   MapShapeSource createMapShapeSource() {
     if (!_isInitialized) {
       return const MapShapeSource.asset(
         'assets/Wards-Boundaries.geojson',
-        shapeDataField: 'ward',);
-    }
-    else {
+        shapeDataField: 'ward',
+      );
+    } else {
       return MapShapeSource.asset(
-          'assets/Wards-Boundaries.geojson',
-          shapeDataField: 'ward',
-          dataCount: _filteredMapData.length,
-          primaryValueMapper: (int index) =>
-              _filteredMapData[index].ward,
-          shapeColorValueMapper: (int index) =>
-              _filteredMapData[index].percent,
-          shapeColorMappers: const [
-            MapColorMapper(
-                from: 0,
-                to: 20,
-                color: Color.fromRGBO(205, 255, 255, 1),
-                text: '{0},{20}'),
-            MapColorMapper(
-                from: 20,
-                to: 40,
-                color: Color.fromRGBO(157, 214, 255, 1),
-                text: '40'),
-            MapColorMapper(
-                from: 40,
-                to: 60,
-                color: Color.fromRGBO(110, 168, 234, 1),
-                text: '60'),
-            MapColorMapper(
-                from: 60,
-                to: 80,
-                color: Color.fromRGBO(61, 125, 188, 1),
-                text: '80'),
-            MapColorMapper(
-                from: 80,
-                to: 100,
-                color: Color.fromRGBO(0, 85, 143, 1),
-                text: '100'),
-          ],
+        _getBoundaryAssetPath(_selectedYear),
+        shapeDataField: 'ward',
+        dataCount: _filteredMapData.length,
+        primaryValueMapper: (int index) => _filteredMapData[index].ward,
+        shapeColorValueMapper: (int index) => _filteredMapData[index].percent,
+        shapeColorMappers: const [
+          MapColorMapper(
+              from: 0,
+              to: 20,
+              color: Color.fromRGBO(205, 255, 255, 1),
+              text: '{0},{20}'),
+          MapColorMapper(
+              from: 20,
+              to: 40,
+              color: Color.fromRGBO(157, 214, 255, 1),
+              text: '40'),
+          MapColorMapper(
+              from: 40,
+              to: 60,
+              color: Color.fromRGBO(110, 168, 234, 1),
+              text: '60'),
+          MapColorMapper(
+              from: 60,
+              to: 80,
+              color: Color.fromRGBO(61, 125, 188, 1),
+              text: '80'),
+          MapColorMapper(
+              from: 80,
+              to: 100,
+              color: Color.fromRGBO(0, 85, 143, 1),
+              text: '100'),
+        ],
       );
     }
   }
@@ -157,73 +159,74 @@ class __ChoroplethMapState extends State<_ChoroplethMap> {
     if (!_isInitialized) {
       return const CircularProgressIndicator();
     }
-      return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(15),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            yearCategorySelector(),
-            Expanded(child: wardMapCreator()),
-          ],
-        ),
-      )
-    );
+    return Scaffold(
+        body: Padding(
+      padding: EdgeInsets.all(15),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          yearCategorySelector(),
+          Expanded(child: wardMapCreator()),
+        ],
+      ),
+    ));
   }
 
   SfMaps wardMapCreator() {
-    return SfMaps(
-            layers: [
-              MapShapeLayer(
-                source: _dataSource, 
-                legend: MapLegend.bar(MapElement.shape, title: Text('Percent of Annual Spending')),
-                shapeTooltipBuilder: (BuildContext context, int index) {
-                return Container(
-                  width: 180,
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Stack(
-                        children: [
-                          Center(
-                            child: Text(
-                              'Ward: ' + _filteredMapData[index].ward,
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: Theme.of(context)
-                                        .textTheme
-                                        .bodyMedium!
-                                        .fontSize),
-                            ),
-                          ),
-                        ],
-                      ),
-                      const Divider(
-                          color: Colors.grey,
-                          height: 10,
-                          thickness: 1.2,
-                      ),
-                      Text(
-                        'Percent Spending: ' + _filteredMapData[index].percent.toStringAsFixed(1) + '%',
+    return SfMaps(layers: [
+      MapShapeLayer(
+        source: _dataSource,
+        legend: MapLegend.bar(MapElement.shape,
+            title: Text('Percent of Annual Spending')),
+        shapeTooltipBuilder: (BuildContext context, int index) {
+          return Container(
+            width: 180,
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Stack(
+                  children: [
+                    Center(
+                      child: Text(
+                        'Ward: ' + _filteredMapData[index].ward,
                         style: TextStyle(
-                        color: Colors.black,
-                        fontSize:
-                          Theme.of(context).textTheme.bodyMedium!.fontSize),
+                            color: Colors.black,
+                            fontSize: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .fontSize),
                       ),
-                    ],
-                  ),
-                );
-              },
-              tooltipSettings: const MapTooltipSettings(
-                color: Colors.white,
-                // strokeColor: Color.fromRGBO(252, 187, 15, 1),
-                strokeWidth: 1.5),
-                ), 
-            ]
+                    ),
+                  ],
+                ),
+                const Divider(
+                  color: Colors.grey,
+                  height: 10,
+                  thickness: 1.2,
+                ),
+                Text(
+                  'Percent Spending: ' +
+                      _filteredMapData[index].percent.toStringAsFixed(1) +
+                      '%',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize:
+                          Theme.of(context).textTheme.bodyMedium!.fontSize),
+                ),
+              ],
+            ),
           );
+        },
+        tooltipSettings: const MapTooltipSettings(
+            color: Colors.white,
+            // strokeColor: Color.fromRGBO(252, 187, 15, 1),
+            strokeWidth: 1.5),
+      ),
+    ]);
   }
+
   Widget yearCategorySelector() {
     final selectedData = Provider.of<SelectedDataMap>(context);
     return Row(
@@ -235,8 +238,8 @@ class __ChoroplethMapState extends State<_ChoroplethMap> {
           onChanged: (int? newValue) {
             selectedData.updateSelectedYear(newValue!);
           },
-          items: _years.reversed.toList()
-              .map<DropdownMenuItem<int>>((int value) {
+          items:
+              _years.reversed.toList().map<DropdownMenuItem<int>>((int value) {
             return DropdownMenuItem<int>(
               value: value,
               child: Text('$value'),
@@ -248,8 +251,7 @@ class __ChoroplethMapState extends State<_ChoroplethMap> {
           onChanged: (String? newValue) {
             selectedData.updateSelectedCategory(newValue!);
           },
-          items: _categories
-              .map<DropdownMenuItem<String>>((String value) {
+          items: _categories.map<DropdownMenuItem<String>>((String value) {
             return DropdownMenuItem<String>(
               value: value,
               child: Text('$value'),
@@ -261,4 +263,18 @@ class __ChoroplethMapState extends State<_ChoroplethMap> {
   }
 }
 
-
+String _getBoundaryAssetPath(int? year) {
+  if (year == null) {
+    return 'assets/Wards-Boundaries.geojson';
+  }
+  if (year < 2003) {
+    throw Exception('Year must be greater than 2002');
+  }
+  if (year < 2015) {
+    return 'assets/Wards-Boundaries-2003-2015.geojson';
+  } else if (year < 2023) {
+    return 'assets/Wards-Boundaries-2015-2023.geojson';
+  } else {
+    return 'assets/Wards-Boundaries.geojson';
+  }
+}
