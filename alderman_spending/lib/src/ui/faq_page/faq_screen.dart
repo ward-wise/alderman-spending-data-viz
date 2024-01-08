f61a15f9b89582a6c25b0de6fe078fea69fcbdeb
Sean MacMullan <sean.macmullan@gmail.com>
2024-01-08 02:10:13 +0000

    Implement hyperlinks in FAQ information.

--- alderman_spending/lib/src/ui/faq_page/faq_screen.dart
+++ alderman_spending/lib/src/ui/faq_page/faq_screen.dart
@@ -17,98 +17,268 @@ class FAQScreen extends StatelessWidget {
   }
 }
 
+const TextStyle style = TextStyle(color: Colors.black);
+
 class FAQPage extends StatelessWidget {
   final List<FAQ> faqs = [
     FAQ(
       question: "What is a ward?",
-      answer:
-          '''The city of Chicago is divided into 50 legislative areas, called “wards”. Each ward contains roughly 50,000 residents and can overlap with multiple neighborhoods. Ward boundaries are redrawn every 8 years and were last redrawn in 2023.
+      answer: RichText(
+        text: TextSpan(
+          style: style,
+          children: [
+            const TextSpan(
+              text:
+                  '''The city of Chicago is divided into 50 legislative areas, called “wards”. Each ward contains roughly 50,000 residents and can overlap with multiple neighborhoods. Ward boundaries are redrawn every 8 years and were last redrawn in 2023.
 
-See the list of wards: 
-https://www.chicago.gov/city/en/about/wards.html
-                  ''',
+See the list of wards:
+''',
+            ),
+            WidgetSpan(
+              child: HyperLink(
+                displayText: 'https://www.chicago.gov/city/en/about/wards.html',
+                link: 'https://www.chicago.gov/city/en/about/wards.html',
+              ),
+            ),
+          ],
+        ),
+      ),
     ),
     FAQ(
       question: "What is an alder?",
-      answer:
-          '''Each ward elects an alder, who is essentially a mini-mayor. They provide ward services and vote on citywide legislation in the Chicago City Council.
+      answer: RichText(
+        text: TextSpan(
+          style: style,
+          children: [
+            const TextSpan(
+              text:
+                  '''Each ward elects an alder, who is essentially a mini-mayor. They provide ward services and vote on citywide legislation in the Chicago City Council.
 
-Learn more about what an alder does: 
-https://www.citybureau.org/newswire/2023/1/24/what-does-an-alderman-do
-                  ''',
+Learn more about what an alder does:
+''',
+            ),
+            WidgetSpan(
+              child: HyperLink(
+                displayText:
+                    'https://www.citybureau.org/newswire/2023/1/24/what-does-an-alderman-do',
+                link:
+                    'https://www.citybureau.org/newswire/2023/1/24/what-does-an-alderman-do',
+              ),
+            ),
+          ],
+        ),
+      ),
     ),
     FAQ(
       question: "What are aldermanic menu funds?",
-      answer: '''
-“Every year, the City of Chicago allocates 'menu money' to each of the 50 alders to spend at their discretion on capital improvements within their ward. In the past, alders received \$1.32 million a year. In 2021, the amount increased to \$1.5 million.
+      answer: RichText(
+        text: TextSpan(
+          style: style,
+          children: [
+            const TextSpan(
+              text:
+                  '''“Every year, the City of Chicago allocates 'menu money' to each of the 50 alders to spend at their discretion on capital improvements within their ward. In the past, alders received \$1.32 million a year. In 2021, the amount increased to \$1.5 million.
     Menu money funds can only be spent on capital projects and not operating costs; for example, they can fund a public bench or street repaving, but it cannot fund an after-school program or snacks. Most broadly, the money can be spent on infrastructure projects on public land. This budget is the main source of funding local infrastructure improvements like:
          - street repaving
          - pedestrian safety projects
          - lighting
-         - traffic calming, and 
+         - traffic calming, and
          - sidewalk repairs
 ”
-Source: PB Chicago
+Source: ''',
+            ),
+            WidgetSpan(
+              child: HyperLink(
+                displayText: 'PB Chicago',
+                link: 'https://www.participatepbchicago.org/pages/menumoney',
+              ),
+            ),
+            const TextSpan(
+              text: '''
+
 
 Alders are provided a list of standard menu items they can implement across their ward. They can choose to spend money on projects that aren’t on the list too.
-                  ''',
+''',
+            ),
+          ],
+        ),
+      ),
     ),
     FAQ(
       question: "What is participatory budgeting?",
-      answer: '''
+      answer: RichText(
+        text: TextSpan(
+          style: style,
+          children: [
+            const TextSpan(
+              text: '''
 Participatory budgeting (PB) is a practice having the community directly decide how to spend public budgets. A typical PB process goes like this:
         1. Residents submit PB project ideas
         2. A group of volunteers evaluates the ideas based on need and feasibility
         3. Residents vote on the final list of ideas to determine which ideas get funding
         4. The ward office works with CDOT to implement the winning project ideas
 
-Only a handful wards currently use PB budgeting, but that number is growing. Learn more about participatory budgeting in Chicago here:  https://www.participatepbchicago.org/pages/pbgeneralinfo
-                  ''',
+Only a handful wards currently use PB budgeting, but that number is growing. 
+Learn more about participatory budgeting in Chicago here: ''',
+            ),
+            WidgetSpan(
+              child: HyperLink(
+                displayText:
+                    'https://www.participatepbchicago.org/pages/pbgeneralinfo',
+                link:
+                    'https://www.participatepbchicago.org/pages/pbgeneralinfo',
+              ),
+            ),
+          ],
+        ),
+      ),
     ),
     FAQ(
       question:
           "How can I influence menu funds and infrastructure spending in my ward?",
-      answer: '''
-Reach out to your ward office! 
+      answer: RichText(
+        text: const TextSpan(
+          style: style,
+          children: [
+            TextSpan(
+              text: '''
+Reach out to your ward office!
 You can use our Find My Ward tool to get their contact information. If your ward does participatory budgeting, ask how you can be involved. If not, don’t despair! You can still contact your alder to discuss your infrastructure idea (and ask them to consider participatory budgeting).
 
 Two pieces of advice:
-      1. Phone calls are generally more impactful than emails. 
+      1. Phone calls are generally more impactful than emails.
       2. Try to find other people who care about the same issue. Convince them to contact your ward office too! Many voices have more influence than a single voice alone.
 
                   ''',
+            ),
+          ],
+        ),
+      ),
     ),
     FAQ(
       question: "Where does your data come from?",
-      answer: '''
-Since 2012, the Chicago Office of Budget and Management has published PDFs of itemized aldermanic menu spending to their Capital Improvement Archive. Our site is using raw data extracted from these PDFs by journalist Jake Smith.
+      answer: RichText(
+        text: TextSpan(
+          style: style,
+          children: [
+            const TextSpan(
+              text:
+                  '''Since 2012, the Chicago Office of Budget and Management has published PDFs of itemized aldermanic menu spending to their ''',
+            ),
+            WidgetSpan(
+              child: HyperLink(
+                displayText: 'Capital Improvement Archive',
+                link:
+                    'https://www.chicago.gov/city/en/depts/obm/provdrs/cap_improve/svcs/cip-archive.html',
+              ),
+            ),
+            const TextSpan(
+                text:
+                    '''. Our site is using raw data extracted from these PDFs by journalist '''),
+            WidgetSpan(
+              child: HyperLink(
+                displayText: 'Jake Smith',
+                link: 'https://jakejeromesmith.wordpress.com/',
+              ),
+            ),
+            const TextSpan(
+              text: '''.
 
-Researcher John C. Ruf submitted a FOIA request to get aldermanic spending data going back to 2005. Those PDFs are available here. We hope to incorporate that data into our site in the near future.
+Researcher John C. Ruf submitted a FOIA request to get aldermanic spending data going back to 2005. Those PDFs are available ''',
+            ),
+            WidgetSpan(
+              child: HyperLink(
+                displayText: 'here',
+                link:
+                    'https://github.com/JohnCRuf/alderman_machine/tree/master/tasks/initialdata/output',
+              ),
+            ),
+            const TextSpan(
+              text:
+                  '''. We hope to incorporate that data into our site in the near future.
 
 The standard menu item descriptions and cost information comes from the 2023 Neighborhood Infrastructure Menu Program packet that was sent out to ward offices.
-
-Download a CSV of the spending data (WIP)
                   ''',
+            ),
+          ],
+        ),
+      ),
     ),
     FAQ(
       question: "Can I get involved with Ward Wise?",
-      answer: '''
-Sure! Our data analysis and website source code are publicly available on GitHub. Ward Wise is a breakout group under Chi Hack Night. We meet every Tuesday night.
-                  ''',
+      answer: RichText(
+        text: TextSpan(
+          style: style,
+          children: [
+            const TextSpan(
+                text:
+                    '''Sure! Our data analysis and website source code are '''),
+            WidgetSpan(
+              child: HyperLink(
+                displayText: 'publicly available on GitHub',
+                link: 'https://github.com/ward-wise',
+              ),
+            ),
+            const TextSpan(text: '''. Ward Wise is a breakout group under '''),
+            WidgetSpan(
+              child: HyperLink(
+                displayText: 'Chi Hack Night',
+                link: 'https://chihacknight.org/',
+              ),
+            ),
+            const TextSpan(text: '''. We meet every Tuesday night.'''),
+          ],
+        ),
+      ),
     ),
     FAQ(
       question: "Do you have new features planned for the site?",
-      answer: '''
-See our issues list on GitHub.
-                  ''',
+      answer: RichText(
+        text: TextSpan(
+          style: style,
+          children: [
+            const TextSpan(
+              text: '''See our ''',
+            ),
+            WidgetSpan(
+              child: HyperLink(
+                displayText: 'issues list on GitHub.',
+                link:
+                    'https://github.com/ward-wise/alderman-spending-data-viz/issues',
+              ),
+            ),
+          ],
+        ),
+      ),
     ),
     FAQ(
       question: "I have another question not on this list.",
-      answer: '''
-Send us an email at wardwisechicago@gmail.com and we'll do our best to respond!
-                  ''',
+      answer: RichText(
+        text: const TextSpan(
+          style: style,
+          children: [
+            TextSpan(
+              text:
+                  '''Send us an email at wardwisechicago@gmail.com and we'll do our best to respond!''',
+            ),
+          ],
+        ),
+      ),
     ),
-    // Add more FAQs as needed
+    //extra FAQ for spacing
+    FAQ(
+      question: "",
+      answer: RichText(
+        text: const TextSpan(
+          style: style,
+          children: [
+            TextSpan(
+              text: '',
+            ),
+          ],
+        ),
+      ),
+    ),
   ];
 
   @override
@@ -140,7 +310,7 @@ Send us an email at wardwisechicago@gmail.com and we'll do our best to respond!
 
 class FAQ {
   final String question;
-  final String answer;
+  final RichText answer;
 
   FAQ({required this.question, required this.answer});
 }
@@ -166,10 +336,7 @@ class FAQTile extends StatelessWidget {
                 height: 10), // Add some spacing between title and subtitle
           ],
         ),
-        subtitle: Text(
-          faq.answer,
-          style: Theme.of(context).textTheme.bodyMedium,
-        ),
+        subtitle: faq.answer,
       ),
     );
   }
